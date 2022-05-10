3c33046343c324ffa275fadede42bfd071ac5e69
Djebreds <refiahmad.djebreds@gmail.com>
2022-05-10 21:26:27 +0700

    update views login and register

--- resources/views/layouts/main-admin.blade.php
+++ resources/views/layouts/main-admin.blade.php
@@ -95,37 +95,6 @@
                             </svg> Directors </a>
                     </li>
                 </ul>
-                {{-- <hr class="hr">
-                <p>ADD DATA</p>
-                <ul class="item-list">
-                    <li>
-                        <a href="/add-film"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                fill="currentColor" class="bi bi-node-plus-fill" viewBox="0 0 20 20">
-                                <path
-                                    d="M11 13a5 5 0 1 0-4.975-5.5H4A1.5 1.5 0 0 0 2.5 6h-1A1.5 1.5 0 0 0 0 7.5v1A1.5 1.5 0 0 0 1.5 10h1A1.5 1.5 0 0 0 4 8.5h2.025A5 5 0 0 0 11 13zm.5-7.5v2h2a.5.5 0 0 1 0 1h-2v2a.5.5 0 0 1-1 0v-2h-2a.5.5 0 0 1 0-1h2v-2a.5.5 0 0 1 1 0z" />
-                            </svg> Film </a>
-                    </li>
-                    <li>
-                        <a href="/add-production"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                fill="currentColor" class="bi bi-node-plus-fill" viewBox="0 0 20 20">
-                                <path
-                                    d="M11 13a5 5 0 1 0-4.975-5.5H4A1.5 1.5 0 0 0 2.5 6h-1A1.5 1.5 0 0 0 0 7.5v1A1.5 1.5 0 0 0 1.5 10h1A1.5 1.5 0 0 0 4 8.5h2.025A5 5 0 0 0 11 13zm.5-7.5v2h2a.5.5 0 0 1 0 1h-2v2a.5.5 0 0 1-1 0v-2h-2a.5.5 0 0 1 0-1h2v-2a.5.5 0 0 1 1 0z" />
-                            </svg> Production </a>
-                    </li>
-                    <li>
-                        <a href="/add-genre"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                fill="currentColor" class="bi bi-node-plus-fill" viewBox="0 0 20 20">
-                                <path
-                                    d="M11 13a5 5 0 1 0-4.975-5.5H4A1.5 1.5 0 0 0 2.5 6h-1A1.5 1.5 0 0 0 0 7.5v1A1.5 1.5 0 0 0 1.5 10h1A1.5 1.5 0 0 0 4 8.5h2.025A5 5 0 0 0 11 13zm.5-7.5v2h2a.5.5 0 0 1 0 1h-2v2a.5.5 0 0 1-1 0v-2h-2a.5.5 0 0 1 0-1h2v-2a.5.5 0 0 1 1 0z" />
-                            </svg> Genre </a>
-                    </li>
-                    <li>
-                        <a href="/add-director"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                fill="currentColor" class="bi bi-node-plus-fill" viewBox="0 0 20 20">
-                                <path
-                                    d="M11 13a5 5 0 1 0-4.975-5.5H4A1.5 1.5 0 0 0 2.5 6h-1A1.5 1.5 0 0 0 0 7.5v1A1.5 1.5 0 0 0 1.5 10h1A1.5 1.5 0 0 0 4 8.5h2.025A5 5 0 0 0 11 13zm.5-7.5v2h2a.5.5 0 0 1 0 1h-2v2a.5.5 0 0 1-1 0v-2h-2a.5.5 0 0 1 0-1h2v-2a.5.5 0 0 1 1 0z" />
-                            </svg> Directors </a>
-                    </li> --}}
                 </ul>
                 <div class="logout">
                     <hr class="hr">
